3a20ef9658a33bbf6681feb6ce13eb1ff568c734
Zander Goodwin <zanderngoodwin@gmail.com>
2026-07-13 21:26:37 -0300

    Opening Cutscene finished
    Beginning to add Dialogue

--- Assets/Scripts/Cutscenes/CutsceneManager.cs
+++ Assets/Scripts/Cutscenes/CutsceneManager.cs
@@ -5,8 +5,14 @@ public class CutsceneManager : MonoBehaviour
     public GameObject thrust;
     public float thrustBrightness;
 
+    private Animator anim;
+
     private void Start()
     {
+        anim = GetComponent<Animator>();
         thrust.GetComponent<Renderer>().material.color = new Color(thrustBrightness, thrustBrightness, thrustBrightness);
+
+        anim.Play("Turbulance", 0, Random.value);
+        anim.Update(0);
     }
 }
